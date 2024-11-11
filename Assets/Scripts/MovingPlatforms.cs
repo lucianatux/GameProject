b4bd7b34a8efe349796e8f61922f643b0bf94cfc
lucianatux <lucianatux@yahoo.com.ar>
2024-11-10 21:33:21 -0300

    animations

--- Assets/Scripts/MovingPlatforms.cs
+++ Assets/Scripts/MovingPlatforms.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class MovingPlatforms : MonoBehaviour
 {
+    private Transform[] movementPoints;
+    private float movementVelocity;
+    
     // Start is called before the first frame update
     void Start()
     {
