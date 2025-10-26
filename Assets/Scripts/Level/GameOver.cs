50e805024dfe3e84bed4904d95543e184fcf1f12
Lucianatux <lucianatux@yahoo.com.ar>
2025-10-26 17:29:49 -0300

    last version

--- Assets/Scripts/Level/GameOver.cs
+++ Assets/Scripts/Level/GameOver.cs
@@ -2,15 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro; 
+using UnityEngine.SceneManagement; 
+
 
 public class GameOver : MonoBehaviour
 {
    // Referencia al componente TextMeshPro o Text de la UI
     public TextMeshProUGUI textUI;  // Cambia a Text si usas el sistema tradicional de Unity
     public string message = "Level 2 Completed! To be continued...";  // El mensaje que se va a mostrar
-    public float time = 2f;
+    public float displayTime = 2f;
+    public float returnToMenuTime = 5f;  // Tiempo antes de regresar al menú principal
 
-    // Llama este método cuando otro collider entra en el trigger
+
+ 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // Verifica si el objeto que colisiona tiene la etiqueta "Player"
@@ -19,17 +23,22 @@ public class GameOver : MonoBehaviour
             // Activa el texto de la UI
             textUI.text = message;  // Asigna el mensaje al texto
             textUI.gameObject.SetActive(true);  // Activa el GameObject que contiene el texto
+
+            // Oculta el texto después del tiempo especificado
+            Invoke("HideText", displayTime);
+
+            // Regresa al menú principal después del tiempo especificado
+            Invoke("ReturnToMenu", returnToMenuTime);
         }
     }
 
-    // que el texto desaparezca después de un tiempo
-    public void HideTextAfterTime(float time)
-    {
-        Invoke("HideText", time);
-    }
-
-    private void HideText()
+     private void HideText()
     {
         textUI.gameObject.SetActive(false);  // Desactiva el texto después del tiempo
+    }
+
+    private void ReturnToMenu()
+    {
+        SceneManager.LoadScene("Menu");  // Cambia "Menu" al nombre exacto de tu escena de menú
     }
 }
--- Assets/Scripts/UIScripts/MainMenu.cs
+++ Assets/Scripts/UIScripts/MainMenu.cs
@@ -2,11 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioMixer audioMixer;
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+
+    public void SetVolume(float volume)
+    {
+        Debug.Log(volume);
+        audioMixer.SetFloat("volume", volume);
     }
 }
